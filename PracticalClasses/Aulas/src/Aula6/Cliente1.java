1296d846f024f553d46ab54e262de5dac2b6bb35
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2023-03-30 08:51:29 +0100

    Aula 6 - exc 1 / 2

---
+++ PracticalClasses/Aulas/src/Aula6/Cliente1.java
@@ -0,0 +1,25 @@
+package Aula6;
+
+import java.net.*;
+import java.io.*;
+
+public class Cliente1{
+	private Socket s;
+	public Cliente1() {
+	    try {
+	        s = new Socket("127.0.0.1", 5432);
+	        ObjectInputStream is = new ObjectInputStream(s.getInputStream());
+	        for (int i = 0; i < 5; i++) {
+	            System.out.println(is.readObject());
+	        }
+	        s.close();
+	    } catch (IOException | ClassNotFoundException e) {
+	        System.out.println(e.getMessage());
+	    }
+	}
+
+	public static void main (String args []){
+		Cliente1 c = new Cliente1();
+	}
+}
+
