0f1f04a87d4fb89146ad08576c4a79737f6335b4
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2023-03-30 13:31:29 +0100

    Comments no código do server multithread.

--- PracticalClasses/Aulas/src/Aula3/Client5.java
+++ PracticalClasses/Aulas/src/Aula3/Client5.java
@@ -6,22 +6,31 @@ import java.util.Scanner;
 
 public class Client5 {
 	public static void main(String[] args) throws IOException {
+		// Specifies the IP address and port number to connect to the server
 		String serverAddress = "127.0.0.1";
 		int portNumber = 2222;
 
 		try {
+			// Creates a new socket with the specified server address and port number
 			Socket socket = new Socket(serverAddress, portNumber);
 			
+			// Creates a buffered reader to read input from the server
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			// Creates a print writer to send output to the server
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			// Creates a buffered reader to read input from the keyboard
 			BufferedReader keyboardIn = new BufferedReader(new InputStreamReader(System.in));
+			
+			// Reads the first message sent by the server and prints it to the console
 			String inputLine = in.readLine();
-
 			System.out.println(inputLine);
+			
+			// If the server reached maximum capacity, closes the connection and exits the program
 			if (inputLine.equals("Server reached maximum capacity, try again later.")) {
 				System.out.println("Closing connection...");
 		        socket.close();
 			} else {
+				// Otherwise, enters a loop to continuously prompt the user for input
 				while (true) {
 					System.out.println();
 		            System.out.println("1. Registar aluno");
@@ -32,10 +41,11 @@ public class Client5 {
 		            System.out.print("Enter your choice (1-4): ");
 		            String choice = keyboardIn.readLine();	            
 			
-					// Envia escolha para o servidor
+		            // Sends the user's input to the server
 					out.println(choice);
 					out.flush();
 					
+					// If the user chooses to register a new student
 					String nome = "";
 					switch (choice) {
 			            case "1":
@@ -76,7 +86,9 @@ public class Client5 {
 					        break;
 					}
 
+					// Reads and prints any output sent by the server
 					while ((inputLine = in.readLine()) != null) {
+						// The server sends "EOF" to indicate the end of a message
 						if (inputLine.equals("EOF")) {
 					        break;
 					    }
--- PracticalClasses/Aulas/src/Aula3/Server5.java
+++ PracticalClasses/Aulas/src/Aula3/Server5.java
@@ -9,11 +9,16 @@ import java.io.*;
 import java.lang.Math;
 
 public class Server5 {
-	static ArrayList<Aluno> listaAlunos = readAlunosFromFile();
-	static int numAcessos = 0;
 	static String svHeader = "\n_______________________\n|        SERVER       |\n\n";
 	static String svFooter = "|                     |\n_______________________\nEOF";
+	
+	static ArrayList<Aluno> listaAlunos = readAlunosFromFile();
+	static int numAcessos = 0;
+	
+	// Initialize a Semaphore with a maximum of 5 permits (client connections)
 	static Semaphore sem = new Semaphore(5);
+	
+	// Initialize a thread pool with a fixed number of 5 threads
 	static ExecutorService pool = Executors.newFixedThreadPool(5);
 
 	
@@ -21,20 +26,31 @@ public class Server5 {
         int portNumber = 2222;
 
         try {
+        	// Create a new ServerSocket on the specified port number
             ServerSocket serverSocket = new ServerSocket(portNumber);
+            
             System.out.println("Server started on port " + portNumber);
 
             while (true) {
+            	// Wait for an incoming client connection
             	Socket clientSocket = serverSocket.accept();
-            	if (sem.tryAcquire()) { 
+            	
+            	 // Try to acquire a permit from the semaphore
+            	if (sem.tryAcquire()) {
+            		// If successful, increment the number of accesses and print a message{ 
                     numAcessos++;
                     System.out.println("Client " + clientSocket.getInetAddress().getHostAddress() + " connected");
+                    
+                    // Create a new PrintWriter to send a message to the client
                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                     out.println("Connected to the server " + serverSocket.getInetAddress().getHostAddress());
-                    out.println("EOF");
+                    
+                    // Create a new thread to handle the client connection using the ClientHandler class
                     pool.execute(new ClientHandler(clientSocket));
                 } else {
+                	// If the maximum number of connections has been reached, reject the connection and send a message to the client
                     System.out.println("Maximum number of clients reached, rejecting connection...");
+                    
                     // Close the connection with the client that tried to connect
                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                     out.println("Server reached maximum capacity, try again later.");
@@ -56,32 +72,45 @@ public class Server5 {
 
         public void run() {
             try {
+            	// create BufferedReader and PrintWriter to read and write data to and from the client
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 
                 String inputLine;
                 
+                // loop to read messages from the client until the client sends the message "5" or the connection is closed
                 while ((inputLine = in.readLine()) != null) {
+                	// print the received message to the console
                 	System.out.println("Client: Option=" + inputLine);
+                	
+                	// if the received message is "5", break out of the loop and close the connection
                 	if (inputLine.equalsIgnoreCase("5")) {
     			        break;
     			    }
+                	
                 	String nome = "";
-                
+                	
+                	// use a switch statement to handle different types of messages
                 	switch (inputLine) {
 	    	            case "1":
 	    	            	synchronized(listaAlunos) {
+	    	            		// read additional data from the client
+	    	            		
 	    	            		String numero = in.readLine();
 	    	            		nome = in.readLine();
 	    	            		String curso = in.readLine();
 	    	            		String telemovel = in.readLine();
 	    	            		String email = in.readLine();
 
+	    	            		// check if the student is already registered
 	    	            		Boolean alunosRepetidos = verificaRepetidos(Integer.parseInt(numero), listaAlunos);
 
+	    	            		// if the student is already registered, send a message to the client indicating that the registration failed
 	    	            		if(alunosRepetidos) {
 	    	            			out.println(svHeader + "Aluno repetido. Não foi registado novamente. Número de alunos registados: " + listaAlunos.size() + ".\n\n"+svFooter);
-	    	            		} else {
+	    	            		}
+	    	            		// if the student is not already registered, add the student to the list and send a message to the client indicating that the registration succeeded
+	    	            		else {
 	    	            			Aluno novo = new Aluno(Integer.parseInt(numero), nome, curso, Integer.parseInt(telemovel), email);
 	    	            			listaAlunos.add(novo);
 	    	            			writeAlunosToFile(listaAlunos);
@@ -91,15 +120,20 @@ public class Server5 {
 	    	            	}
 	    	                break;
 	    	            case "2":
+	    	            	// send a list of students to the client
 	    	            	out.println(outputListaDeAlunos(listaAlunos));
 	    	            	out.flush();
 	    	                break;
 	    	            case "3":
+	    	            	// send the number of accesses to the server to the client
 	    	            	out.println(svHeader+"Número de acessos ao servidor até ao momento: " + numAcessos + "\n\n"+svFooter);
 	    	            	out.flush();
 	    	                break;
 	    	            case "4":
+	    	            	// read additional data from the client
 	    	            	nome = in.readLine();
+	    	            	
+	    	            	// send the student's phone number to the client
 	    	            	out.println(devolveNumContacto(nome, listaAlunos));
 	    	            	out.flush();
 	    	                break;	    	    	             
@@ -107,21 +141,12 @@ public class Server5 {
                 } 
                 
                 System.out.println("Client " + clientSocket.getInetAddress().getHostAddress() + " disconnected");
+                
+                // release a semaphore used for synchronization and close client socket
                 sem.release();
                 clientSocket.close();
             }   
             catch (IOException e) {
-            	System.err.println("Error: " + e.getMessage());
-
-                // send error message to client
-                try {
-                    Socket errorSocket = new Socket("localhost", 2222);
-                    PrintWriter out = new PrintWriter(errorSocket.getOutputStream(), true);
-                    out.println("Error: " + e.getMessage());
-                    errorSocket.close();
-                } catch (IOException ex) {
-                    System.err.println("Failed to send error message to client: " + ex.getMessage());
-                }
             }
         }
     }
