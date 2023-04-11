806db014c3854b1788ff19ccbc58993aeb3ea045
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2023-04-11 12:19:48 +0100

    Aula prática 7. Faltam os dois últimos exercícios.

---
+++ PracticalClasses/Aulas/src/Aula7/RMIImpl.java
@@ -0,0 +1,28 @@
+package Aula7;
+
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+public class RMIImpl extends UnicastRemoteObject implements RMIInterface
+{
+	ArrayList<String> lista;
+	int contadorConsultas = 0;
+	
+	public RMIImpl() throws java.rmi.RemoteException {
+		super();
+		lista = new ArrayList<String>();
+	}
+	
+	public void adiciona ( String s) throws java.rmi.RemoteException {
+		lista.add(s);
+	}
+	
+	public ArrayList<String> consulta() throws java.rmi.RemoteException{
+		contadorConsultas++;
+		return lista;
+	}
+	
+	public int getCount () throws java.rmi.RemoteException {
+		return contadorConsultas;
+	}
+}
