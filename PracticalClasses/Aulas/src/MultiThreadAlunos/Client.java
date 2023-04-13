d66712450cf60801b7914c3232c911ed5e14d6ef
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2023-04-13 22:27:05 +0100

    Update Client.java

--- PracticalClasses/Aulas/src/MultiThreadAlunos/Client.java
+++ PracticalClasses/Aulas/src/MultiThreadAlunos/Client.java
@@ -1,81 +1,95 @@
 package MultiThreadAlunos;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Client {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		// Set server IP and port number
 		String serverAddress = "127.0.0.1";
 		int portNumber = 2222;
 
 		try {
+			// Connect to server
 			Socket socket = new Socket(serverAddress, portNumber);
-			
+
+			// Initialize input and output streams
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
+			// Read welcome message from server
 			String inputLine = in.readLine();
 			System.out.println(inputLine);
 
+			// If server is full, close the connection
 			if (inputLine.equals("Server reached maximum capacity, try again later.")) {
 				System.out.println("Closing connection...");
-		        socket.close();
+				socket.close();
 			} else {
+				// Loop until user quits
 				while (true) {
 					System.out.println();
-		            System.out.println("1. Registar aluno");
-		            System.out.println("2. Consultar alunos");
-		            System.out.println("3. Consultar acessos");
-		            System.out.println("4. Ver dados de aluno");
-		            System.out.println("5. Quit");
-		            System.out.print("Enter your choice (1-4): ");
-		            String choice = Ler.umaString();	            
-	
+					System.out.println("1. Register student");
+					System.out.println("2. Consult students");
+					System.out.println("3. Consult accesses");
+					System.out.println("4. View student data");
+					System.out.println("5. Quit");
+					System.out.print("Enter your choice (1-4): ");
+
+					// Read user input
+					String choice = new BufferedReader(new InputStreamReader(System.in)).readLine();
+
+					// Send user choice to server
 					out.println(choice);
 					out.flush();
 
-					String nome = "";
+					// Process user choice
 					switch (choice) {
-			            case "1":
-			            	System.out.print("\nNumero: ");
-			            	String numero = Ler.umaString();
-			            	out.println(numero);
-			            	out.flush();
-			            	
-			            	System.out.print("Nome: ");
-			            	nome = Ler.umaString();
-			            	out.println(nome);
-			            	out.flush();
-			            	
-			            	System.out.print("Curso: ");
-			            	String curso = Ler.umaString();
-			            	out.println(curso);
-			            	out.flush();
-			            	
-			            	System.out.print("Telemovel: ");
-			            	String telemovel = Ler.umaString();
-			            	out.println(telemovel);
-			            	out.flush();
-			            	
-			            	System.out.print("Email: ");
-			            	String email = Ler.umaString();
-			            	out.println(email);
-			            	out.flush();
-			                break;
-			            case "4":
-			            	System.out.print("\nNome: ");
-			            	nome = Ler.umaString();
-			            	out.println(nome);
-			            	out.flush();
-			            	break;
-			            case "5":
-			            	System.out.println("Closing connection...");
-					        socket.close();
-					        break;
+					case "1":
+						// Register student
+						System.out.print("\nNumber: ");
+						String number = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						out.println(number);
+
+						System.out.print("Name: ");
+						String name = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						out.println(name);
+
+						System.out.print("Course: ");
+						String course = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						out.println(course);
+
+						System.out.print("Phone number: ");
+						String phone = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						out.println(phone);
+
+						System.out.print("Email: ");
+						String email = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						out.println(email);
+						break;
+
+					case "4":
+						// View student data
+						System.out.print("\nName: ");
+						String studentName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+						out.println(studentName);
+						break;
+
+					case "5":
+						// Quit
+						System.out.println("Closing connection...");
+						socket.close();
+						return;
 					}
 
+					// Read server response
 					while ((inputLine = in.readLine()) != null) {
-						if (inputLine.equals("EOF")) { break; }
+						if (inputLine.equals("EOF")) {
+							break;
+						}
 						System.out.println(inputLine);
 					}
 				}
