d3a2454c4e6220da6ca628dadbfc3e977dc5a4c4
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2023-04-23 15:41:36 +0100

    Factories

--- PracticalClasses/Aulas/src/Aula8/CidadeFactoryImpl.java
+++ PracticalClasses/Aulas/src/Aula8/CidadeFactoryImpl.java
@@ -1,24 +1,29 @@
 package Aula8;
 
+import java.rmi.RemoteException;
+
 public class CidadeFactoryImpl extends java.rmi.server.UnicastRemoteObject
 implements CidadeFactory {
+	
 	public CidadeFactoryImpl() throws java.rmi.RemoteException {
 		super();
 	}
+	
 	public Cidade getServidorCidade (String nomeCidade)
 			throws java.rmi.RemoteException{
 		CidadeImpl ServidorCidade = new CidadeImpl(nomeCidade);
 		return (Cidade) ServidorCidade;
 	}
+	
 	public static void main (String arg[]){
-		java.rmi.registry.LocateRegistry.createRegistry(1099);
 		try {
+			java.rmi.registry.LocateRegistry.createRegistry(1099);
 			CidadeFactory factory = new CidadeFactoryImpl ();
 			java.rmi.Naming.rebind("CidadeFactory", factory) ;
 			System.out.println( "CidadeFactory registada");
 		}
 		catch (Exception e ){
 			System.out.println( e.getMessage());
-		} 
+		}
 	}
 }
