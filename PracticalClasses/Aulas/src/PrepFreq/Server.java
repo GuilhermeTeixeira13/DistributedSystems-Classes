869d5702d7b8fa4aa14904998a708e7b47874b0a
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2023-05-19 23:20:55 +0100

    Exc 1 ficha preparação para a freq

---
+++ PracticalClasses/Aulas/src/PrepFreq/Server.java
@@ -0,0 +1,36 @@
+package PrepFreq;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	private ServerSocket ss;
+	private Socket s;
+	private Connection c;
+	private int numConnections = 0;
+	
+	public Server() {
+		try {
+			ss = new ServerSocket (2222);
+		}
+		catch ( IOException e){
+			System.out.println(e.getMessage());
+		}
+		try {
+			while (true) {
+				numConnections++;
+				s = ss.accept();
+				c = new Connection ( s, numConnections ); 
+			} 
+		
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main (String args[]){
+		Server server = new Server();
+	}
+
+}
